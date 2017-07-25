fa181371050f937d89a0fe5f1bb429682a28c4c3
repairtrackerutm <repairtrackerutm@gmail.com>
2017-07-25 14:13:18 +0400

    models updated

--- common/models/sms-inbox.js
+++ common/models/sms-inbox.js
@@ -1,5 +1,22 @@
 'use strict';
 
 module.exports = function(Smsinbox) {
+  Smsinbox.deleteInboxGreaterId = function(minId,cb) {
+    Smsinbox.destroyAll( {id: {gte: minId}}, function (err, info) {
+      var response = "Num of deletes : "+info.count;
+
+      cb(null, response);
+      //console.log(response);
+    });
+  }
+
+    Smsinbox.remoteMethod (
+      'deleteInboxGreaterId',
+      {
+        http: {path: '/deleteInboxGreaterId', verb: 'post'},
+        accepts: {arg: 'id', type: 'string', http: { source: 'query' } },
+        returns: {arg: 'output', type: 'string'}
+      }
+    );
 
 };
